62c64365b41a0807040362278eebb076b7acaa50
Ghost <duchojakub987@gmail.com>
2026-03-02 08:04:13 +0100

    white background

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,7 +1,7 @@
 #include "engine.hpp"
 
 void draw_circle(SDL_Renderer *renderer, int center_x, int center_y, int radius) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     int x, y;
     for (int angle = 0; angle < 360; angle++) {
         double rad_angle = angle * M_PI / 180.0; 
@@ -16,13 +16,18 @@ void Engine::run(RendererWindow *window) {
 
     SDL_Renderer *renderer = window->get_renderer();
     SDL_Texture *canvas = window->get_canvas();
+    SDL_SetRenderTarget(renderer, canvas);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderTarget(renderer, NULL);
+
     SDL_Event event;
 
     std::vector<Button> buttons;
     Button test(50, 100, 100, 50, "Test");
     buttons.push_back(test);
+
     SDL_SetRenderTarget(renderer, canvas);
-    SDL_SetRenderDrawColor(renderer, 50, 100, 255, 255);
     draw_ui(buttons, renderer);
 
     bool running = true;
--- src/ui/ui_manager.cpp
+++ src/ui/ui_manager.cpp
@@ -1,6 +1,7 @@
 #include "ui_manager.hpp"
 
 void draw_ui(const std::vector<Button> &buttons, SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 50, 100, 255, 255);
     for (Button btn : buttons) {
         btn.draw_button(renderer);
     }
